3003e5b585f1bb53c50d91b8697b33989201bca0
JoseRinaldi <jose.maria.rinaldi@gmail.com>
2023-02-01 08:04:16 -0300

    validaciones 1

--- dominio/Trainee.cs
+++ dominio/Trainee.cs
@@ -9,7 +9,23 @@ namespace dominio
      public class Trainee
     {
         public int Id { get; set; }
-        public string Email { get; set; }
+        private string email;//este es un tipo de validacion q la realizamos dentro de la propiedad del objeto
+        public string Email
+
+        {
+            get { return email; }
+            set 
+            {
+                if (value != "")
+                {
+                    email = value;
+                }
+                else
+                {
+                    throw new Exception("El email esta vacio en el dominio ....");
+                }
+            } 
+        }
         public string Pass { get; set; }
         public string Nombre { get; set; }
         public string Apellido { get; set; }
